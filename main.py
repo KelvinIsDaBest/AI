76c04b4d64a14a9f047dd44f6d0f7523e362bd76
notkelvin <chewkelvin1704@gmail.com>
2025-09-01 21:30:39 +0800

    rmv file

--- main.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(
-    page_title="Large-Scale Movie Reviews Sentiment Analysis Through TF-IDF, POS-Driven Phrase-Level Feature Engineering and Transformer",
-    page_icon="🍴"
-)
-
-st.title("Main Page")
-st.sidebar.success("nlp-movie-review.py")
