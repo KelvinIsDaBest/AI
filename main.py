486d194a36c79d0783b33ad3e4fbc3f164d434dc
notkelvin <chewkelvin1704@gmail.com>
2025-09-01 21:17:46 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Large-Scale Movie Reviews Sentiment Analysis Through TF-IDF, POS-Driven Phrase-Level Feature Engineering and Transformer"
+    page_icon="🍴"
+)
+
+st.title("Main Page")
+st.sidebar.success("Select a page above")
